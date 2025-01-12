86234ec5bb29daacf7842cb167cdd5bfa7769838
Neacsu Mihnea <neomihnea@R.Mihnea>
2025-01-12 23:17:47 +0200

    Implemented rate-monotonic

--- core/proc.go
+++ core/proc.go
@@ -1,23 +1,33 @@
 package core
 
 type Proc struct {
-	Pid       int
-	Burst     int
-	InitBurst int
-	Arrive    int
-	Priority  int
-
-	// -1 for no period
+	Pid          int
+	Burst        int
+	InitBurst    int
+	Arrive       int
+	Priority     int
+	ParentPeriod int //for the real-time algorithms, we'll need to know how often it's made
+}
+type ProcGenerator struct {
+	Id     int
+	Burst  int
 	Period int
 }
 
-func NewProc(pid, burst, arrive, priority, period int) *Proc {
+func NewProc(pid, burst, arrive, priority, pperiod int) *Proc {
 	return &Proc{
 		pid,
 		burst,
 		burst,
 		arrive,
 		priority,
+		pperiod,
+	}
+}
+func NewGenerator(id, burst, period int) *ProcGenerator {
+	return &ProcGenerator{
+		id,
+		burst,
 		period,
 	}
 }
